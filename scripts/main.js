fb68a56a89f1c8d00247d5083b337e9bdb74366e
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-02-22 16:35:13 -0700

    added image array

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const jeeps = [
+  "assets/images/couple-jeeps.jpg",
+  "assets/images/jeep-background.jpg",
+  "assets/images/jeep-profile-makayla.jpg",
+  "assets/images/jeep-profile-nathan.jpg",
+  "assets/images/jeepjk-bctrip.jpg",
+  "assets/images/jeepjk-jeeptj-airdown.jpg",
+  "assets/images/jeeptj-airdown.jpg",
+  "assets/images/jeeptj-hole.jpg",
+  "assets/images/jeeptj-sideprofile.jpg"
+];
