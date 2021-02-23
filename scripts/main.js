3dd35ca767ae92b5233dca8d6fd7fbc91707cff1
MakaylaMR <martineauraymundomakayla@gmail.com>
2021-02-23 07:59:26 -0700

    added things to define objects

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,41 @@
 'use strict'
 
 const jeeps = [
-  "assets/images/couple-jeeps.jpg",
-  "assets/images/jeep-background.jpg",
-  "assets/images/jeep-profile-makayla.jpg",
-  "assets/images/jeep-profile-nathan.jpg",
-  "assets/images/jeepjk-bctrip.jpg",
-  "assets/images/jeepjk-jeeptj-airdown.jpg",
-  "assets/images/jeeptj-airdown.jpg",
-  "assets/images/jeeptj-hole.jpg",
-  "assets/images/jeeptj-sideprofile.jpg"
+  {
+    path: 'assets/images/couple-jeeps.jpg',
+    alt: 'Two Jeeps'
+  },
+  {
+    path: 'assets/images/jeep-background.jpg',
+    alt: 'Two Jeeps'
+  },
+  {
+    path:'assets/images/jeep-profile-makayla.jpg',
+    alt: 'jeeps'
+  },
+  {
+    path:'assets/images/jeep-profile-nathan.jpg',
+    alt: 'jeeps'
+  },
+  {
+    path: 'assets/images/jeepjk-bctrip.jpg',
+    alt:'jeeps in bc'
+  },
+  {
+    path: 'assets/images/jeepjk-jeeptj-airdown.jpg',
+    alt:'jeeps'
+  },
+  {
+    path:'assets/images/jeeptj-airdown.jpg',
+    alt:'jeeps'
+  },
+  {
+    path:'assets/images/jeeptj-hole.jpg',
+    alt:'jeeps'
+  },
+  {
+    path:'assets/images/jeeptj-sideprofile.jpg',
+    alt:'jeeps'
+  }
 ];
+
